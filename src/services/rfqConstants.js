d1b9ac442fcf9a10744ed31623d9c16c6f97e1ae
ckhbtc <github@ckh.email>
2026-05-26 01:32:05 +0100

    feat(rfq): define mainnet endpoints

---
+++ src/services/rfqConstants.js
@@ -0,0 +1,7 @@
+export const RFQ_CONTRACT_ADDRESS = 'inj1e0u9nl50gzhmrmhwx3v9vf535vkhwzpwku6mtk';
+export const RFQ_WS_URL = 'wss://rfq.ws.injective.network';
+export const RFQ_GATEWAY_URL = 'https://rfq.gateway.grpc-web.injective.network/';
+export const RFQ_CHAIN_ID = 'injective-1';
+export const RFQ_EVM_CHAIN_ID = 1776;
+export const RFQ_COLLECT_QUOTES_MS = 500;
+export const RFQ_REQUEST_TIMEOUT_MS = 15_000;
